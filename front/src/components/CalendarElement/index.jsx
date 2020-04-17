2a9b6ebe996b9db21c898b8166b13145e4d33daf
shodayuki <yshoda1227@gmail.com>
2020-04-17 22:14:40 +0900

    曜日と当月表示/todayの表示のリファクタリング

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import * as styles from "./style.css";
 import { Typography } from "@material-ui/core";
 import dayjs from "dayjs";
+import { isSameMonth, isFirstDay, isSameDay } from "../../services/calendar";
 
 const CalendarElement = ({ day }) => {
-  const isFirstDay = day.date() === 1;
-  const format = isFirstDay ? "M月D日" : "D";
   const today = dayjs();
-  const compareFormat = "YYYYMMDD";
-  const isToday = day.format(compareFormat) === today.format(compareFormat);
-  const isCurrentMonth = day.month() === today.month();
+  const isCurrentMonth = isSameMonth(day, today);
   const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+  const format = isFirstDay(day) ? "M月D日" : "D";
+  const isToday = isSameDay(day, today);
 
   return (
     <div className={styles.element}>
