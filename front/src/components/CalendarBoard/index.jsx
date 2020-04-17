53d6622a26675c33df9becb082bcff051bbf8530
shodayuki <yshoda1227@gmail.com>
2020-04-17 21:49:42 +0900

    曜日と当月を表示/todayのスタイル変更

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GridList, StylesProvider } from "@material-ui/core";
+import { GridList, Typography } from "@material-ui/core";
 import * as styles from "./style.css";
 
 import CalendarElement from "../CalendarElement";
@@ -7,13 +7,28 @@ import { createCalendar } from "../../services/calendar";
 
 const calendar = createCalendar();
 
+const days = ["日", "月", "火", "水", "木", "金", "土"];
+
 const CalendarBoard = () => {
   return (
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map(d =>
+          <li ley={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+        )}
         {calendar.map(c => (
           <li key={c.toISOString()}>
-            <CalendarElement>{c.format("D")}</CalendarElement>
+            <CalendarElement day={c} />
           </li>
         ))}
       </GridList>
--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import * as styles from "./style.css";
-import CalendarBoard from "../CalendarBoard";
+import { Typography } from "@material-ui/core";
+import dayjs from "dayjs";
 
-const CalendarElement = ({ children }) => {
+const CalendarElement = ({ day }) => {
+  const isFirstDay = day.date() === 1;
+  const format = isFirstDay ? "M月D日" : "D";
+  const today = dayjs();
+  const compareFormat = "YYYYMMDD";
+  const isToday = day.format(compareFormat) === today.format(compareFormat);
+  const isCurrentMonth = day.month() === today.month();
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
   return (
     <div className={styles.element}>
-      <div className={styles.date}>{children}</div>
+      <Typography
+        className={styles.date}
+        color={textColor}
+        align="center"
+        variant="caption"
+        component="div"
+      >
+        <span className={isToday ? styles.today : ""}>
+          {day.format(format)}
+        </span>
+      </Typography>
     </div>
   );
 };
